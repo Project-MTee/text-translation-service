96fb5a597ee4e1cd84f822f3233422317a95ab0d
Guntars Pužulis <guntars.puzulis@tilde.lv>
2021-09-30 14:49:03 +0300

    Update docker entrypoint

--- TranslationAPIService/Models/RabbitMQ/RabbitMQGenericMessage.cs
+++ TranslationAPIService/Models/RabbitMQ/RabbitMQGenericMessage.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace Tilde.MT.TranslationAPIService.Models.RabbitMQ
 {
@@ -11,12 +7,12 @@ namespace Tilde.MT.TranslationAPIService.Models.RabbitMQ
         /// <summary>
         /// HTTP status code (integer), 200 by default
         /// </summary>
-        [JsonPropertyName("status_code")]
+        [JsonProperty("status_code")]
         public int StatusCode { get; set; }
         /// <summary>
         /// human-readable error message or "OK"
         /// </summary>
-        [JsonPropertyName("status")]
+        [JsonProperty("status")]
         public string Status { get; set; }
     }
 }
