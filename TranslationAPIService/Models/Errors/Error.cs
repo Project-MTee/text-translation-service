822b0e52c2f8016783b126f6329fd77ae41f6963
Guntars Pužulis <guntars.puzulis@tilde.lv>
2021-11-14 12:34:51 +0200

    Fix documentation

--- TranslationAPIService/Models/Errors/Error.cs
+++ TranslationAPIService/Models/Errors/Error.cs
@@ -6,9 +6,9 @@ namespace Tilde.MT.TranslationAPIService.Models.Errors
     public class Error
     {
         /// <summary>
-        /// Error code
+        /// Error code consisting of HTTP status code and error ID
         /// </summary>
-        /// <example>500</example>
+        /// <example>500001</example>
         [JsonPropertyName("code")]
         public int Code { get; set; }
 
